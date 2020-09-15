c571db7d73bc84e20861c4a4a47c69f64b62ee19
Marina <marina.ssaito@gmail.com>
2020-09-14 21:32:24 -0300

    professor

---
+++ src/br/com/digitalhouse/revisao/Professor.kt
@@ -0,0 +1,8 @@
+package br.com.digitalhouse.revisao
+
+class Professor(var rd : Int, nome : String, sobrenome : String) : Pessoa(nome, sobrenome) {
+
+    fun darAulas() {}
+
+    fun fazerChamada() {}
+}
